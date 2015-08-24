7798724cfbafc0fda9ef2f66164bac31b79a6075
pengyouyi <2506644844@qq.com>
2015-08-24 17:11:31 +0800

    success drag

--- drag.js
+++ drag.js
@@ -6,12 +6,15 @@ function drag(id){
 		var ev=ev || event;
 		disX=ev.pageX-obj.offsetLeft;
 		disY=ev.pageY-obj.offsetTop;
-		document.onmousemove=function(ev){
 
+		document.onmousemove=function(ev){
 			var ev=ev || event ;
 			obj.style.left=ev.pageX-disX+'px';
 			obj.style.top=ev.pageY-disY+'px';
-
-		}
+		};
+		document.onmouseup=function(ev){
+			document.onmouseup=document.onmousemove=null;
+		};
 	}
+	return false;
 }
