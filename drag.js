398afdba361222d2e3b1e21b8e98573d3523ad20
pengyouyi <2506644844@qq.com>
2015-08-24 16:11:43 +0800

    change drag.js from new1

--- drag.js
+++ drag.js
@@ -6,5 +6,10 @@ function drag(id){
 		var ev=ev || event;
 		disX=ev.pageX-obj.offsetLeft;
 		disY=ev.pageY-obj.offsetTop;
+		document.onmousemove=function(ev){
+			var ev=ev || event;
+			obj.style.left=ev.clientX-disX+'px';
+			obj.style.top=ev.clientY-disY+'px';
+		}
 	}
 }
