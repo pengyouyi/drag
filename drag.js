079ae3346739c03d158a444711a4a766270146b3
pengyouyi <2506644844@qq.com>
2015-08-24 10:53:06 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -2,7 +2,9 @@ function drag(id){
 	var obj=document.getElementById(id);
 	var disX=0;
 	var disY=0;
-	obj.onmousedown=function(){
-		
+	obj.onmousedown=function(ev){
+		var ev=ev || event;
+		disX=ev.pageX-obj.offsetLeft;
+		disY=ev.pageY-obj.offsetTop;
 	}
 }
